106934cdd9cd1df463e35c12ab5cfa785c9a32e0
Barre Dijkstra <barre.dijkstra@qsd.nl>
2013-07-04 07:34:31 +0200

    Added unit test for FixtureFitting Added some API documentation

--- fitting-core/src/main/java/org/fitting/ByProvider.java
+++ fitting-core/src/main/java/org/fitting/ByProvider.java
@@ -1,7 +1,25 @@
 package org.fitting;
 
+/**
+ * Provider for creating {@link By}-clause instances.
+ */
 public interface ByProvider {
-    By getBy(String byTag, String query);
+    /**
+     * Get the By-clause with a given tag.
+     *
+     * @param byTag The tag fo the clause.
+     * @param query The query for the By-clause.
+     *
+     * @return The By-clause.
+     *
+     * @throws FittingException When no matching By-clause could be found.
+     */
+    By getBy(String byTag, String query) throws FittingException;
 
+    /**
+     * Get the By-clause tags available via the provider.
+     *
+     * @return The available tags.
+     */
     String[] getAvailableTags();
 }
---
+++ fitting-core/src/test/java/org/fitting/fixture/FittingFixtureTest.java
@@ -0,0 +1,189 @@
+package org.fitting.fixture;
+
+import org.fitting.*;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.fail;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+
+/**
+ * Unit tests for {@link FittingFixture}.
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(FittingContainer.class)
+public class FittingFixtureTest {
+    @Mock
+    private FittingConnector connector;
+    @Mock
+    private ByProvider byProvider;
+    @Mock
+    private SearchContextProvider searchContextProvider;
+    @Mock
+    private By by;
+    @Mock
+    private FittingAction action;
+    @Mock
+    private ElementContainerProvider containerProvider;
+
+    /**
+     * The class under test.
+     */
+    private FittingFixture fixture;
+
+    /**
+     * Set up the test.
+     *
+     * <p>
+     * This method is called before every test method.
+     * </p>
+     */
+    @Before
+    public void setUp() {
+        when(connector.getByProvider()).thenReturn(byProvider);
+        when(connector.getElementContainerProvider()).thenReturn(containerProvider);
+        when(connector.getFittingAction()).thenReturn(action);
+
+        PowerMockito.mockStatic(FittingContainer.class);
+        PowerMockito.when(FittingContainer.get()).thenReturn(connector);
+
+        fixture = new FittingFixture(searchContextProvider) {
+        };
+    }
+
+    /**
+     * Ensure that the correct default search context provider is set upon creation.
+     *
+     * @see FittingFixture#FittingFixture()
+     */
+    @Test
+    public void shouldSetDefaultSearchContextProviderOnCreation() {
+        // TODO Implement once {@link FittingFixture#FittingFixture()} has been implemented with a default search context provider.
+    }
+
+    /**
+     * Ensure that a single search context provider is set on creation.
+     *
+     * @see FittingFixture#FittingFixture(SearchContextProvider...)
+     */
+    @Test
+    public void shouldSetSingleProvidedSearchContextProviderOnCreation() {
+        fail("Implement me!");
+    }
+
+    /**
+     * Ensure that multiple search context providers are set on creation.
+     *
+     * @see FittingFixture#FittingFixture(SearchContextProvider...)
+     */
+    @Test
+    public void shouldSetMultipleProvidedSearchContextProviderOnCreation() {
+        fail("Implement me!");
+    }
+
+    /**
+     * Ensure that providing a null value as a search context provider is not allowed.
+     *
+     * @see FittingFixture#FittingFixture(SearchContextProvider...)
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWhenProvidingNullSearchContextProvider() {
+        new FittingFixture(null) {
+        };
+
+        fail("Managed to create a new fixture with a null search context provider.");
+    }
+
+    /**
+     * Ensure that providing an empty array as search context providers is not allowed.
+     *
+     * @see FittingFixture#FittingFixture(SearchContextProvider...)
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldFailWhenProvidingNoSearchContextProvider() {
+        new FittingFixture(new SearchContextProvider[] {}) {
+        };
+
+        fail("Managed to create a new fixture with no search context providers.");
+    }
+
+    /**
+     * Ensure that a connector is available.
+     *
+     * @see FittingFixture#getConnector()
+     */
+    @Test
+    public void shouldGetConnector() {
+        FittingConnector connector = fixture.getConnector();
+
+        assertNotNull("No connector returned.", connector);
+        assertSame("Different connector returned as expected.", this.connector, connector);
+    }
+
+    /**
+     * Ensure that a By-clause is created for an existing tag.
+     *
+     * @see FittingFixture#getByClause(String, String)
+     */
+    @Test
+    public void shouldCreateByClauseForExistingByTag() {
+        when(byProvider.getBy(eq("tag"), eq("query"))).thenReturn(this.by);
+
+        By by = fixture.getByClause("tag", "query");
+
+        assertNotNull("No By-clause created.", by);
+        assertSame("Different By returned as expected", this.by, by);
+    }
+
+    /**
+     * Ensure that an exception is thrown when a By clause is created for a non-existing tag.
+     *
+     * @see FittingFixture#getByClause(String, String)
+     */
+    @Test(expected = FittingException.class)
+    public void shouldThrowExceptionForCreatingByClauseWithNonExistingByTag() {
+        when(byProvider.getBy(eq("non-existing"), anyString())).thenThrow(FittingException.class);
+
+        fixture.getByClause("non-existing", "query");
+
+        fail("Managed to create a By-clause for a non-existing tag.");
+    }
+
+    /**
+     * Ensure that a FittingAction is available.
+     *
+     * @see FittingFixture#getFittingAction()
+     */
+    @Test
+    public void shouldGetFittingAction() {
+        FittingAction action = fixture.getFittingAction();
+
+        assertNotNull("No FittingAction returned.", action);
+        assertSame("Different FittingAction returned as expected.", this.action, action);
+    }
+
+    /**
+     * Ensure the active container is retrieved.
+     *
+     * @see FittingFixture#getActiveContainer()
+     */
+    @Test
+    public void shouldGetActiveContainer() {
+        fixture.getActiveContainer();
+
+        verify(containerProvider, times(1)).getActiveElementContainer();
+        verifyNoMoreInteractions(containerProvider);
+    }
+}
