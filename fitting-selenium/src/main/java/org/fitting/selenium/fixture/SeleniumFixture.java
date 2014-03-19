b258ff8d4ad1aae5ee66ed1293b49fe55c292e09
Barre Dijkstra <barre.dijkstra@qsd.nl>
2014-03-19 11:09:52 +0100

    Added method for starting a Selenium server in process on the first available port.

--- fitting-selenium/src/main/java/org/fitting/selenium/fixture/SeleniumFixture.java
+++ fitting-selenium/src/main/java/org/fitting/selenium/fixture/SeleniumFixture.java
@@ -22,23 +22,70 @@ package org.fitting.selenium.fixture;
 import org.fitting.FittingContainer;
 import org.fitting.selenium.BrowserConnector;
 import org.fitting.selenium.FittingSeleniumConnector;
+import org.fitting.selenium.SeleniumServerManager;
 
 /**
- * Fixture for initialising the Selenium coupling.
+ * Fixture for initialising the Selenium coupling and starting/navigating browsers.
+ *
+ * @author Barre Dijkstra
+ * @since 1.0
  */
 public class SeleniumFixture {
+    /** The number of ports to try when creating a new in-process Selenium server. */
+    private final static int TRIES_FOR_SELENIUM_PORTS = 20;
+    /** The port used for the in-process selenium server, if any. */
+    private ThreadLocal<Integer> seleniumPort = new ThreadLocal<Integer>();
 
-    public void openBrowserOnHostWithPort(String browser, String host, int port) {
-        FittingContainer.set(new FittingSeleniumConnector(BrowserConnector.builder().withBrowser(browser).onHost(host, port).build()));
+    /**
+     * Open a browser, starting a local selenium server in process, and go to a specific URL.
+     *
+     * @param browser The browser to open. See {@link org.fitting.selenium.Browser}.
+     * @param url     The URL to navigate to.
+     */
+    public void openBrowserFor(String browser, String url) throws Exception {
+        if (seleniumPort.get() == null) {
+            int port = SeleniumServerManager.getInstance().startServerOnFirstAvailablePort(TRIES_FOR_SELENIUM_PORTS);
+            seleniumPort.set(port);
+        }
+        openBrowserOnHostWithPortFor(browser, "localhost", seleniumPort.get(), url);
     }
 
+    /**
+     * Open a browser, connecting to an external selenium running on the provided host and port, and go to a specific URL.
+     *
+     * @param browser The browser to open. See {@link org.fitting.selenium.Browser}.
+     * @param host    The host the selenium server is running on.
+     * @param port    The port the selenium server is running on.
+     * @param url     The URL to navigate to.
+     */
+    public void openBrowserOnHostWithPortFor(String browser, String host, int port, String url) {
+        FittingContainer.set(new FittingSeleniumConnector(BrowserConnector.builder().withBrowser(browser).onHost(host, port).build()));
+        openUrl(url);
+    }
+
+    /**
+     * Navigate the active browser window to the provided URL.
+     *
+     * @param url The URL to navigate to.
+     */
     public void openUrl(final String url) {
+        FittingContainer.get().getElementContainerProvider().navigateElementContainerTo(stripURL(url));
+    }
+
+    /**
+     * Since Selenium passes URLs as full &lt;a&gt;-tags, strip the tags from it.
+     *
+     * @param url The URL to strip.
+     *
+     * @return The stripped URL.
+     */
+    private static final String stripURL(String url) {
         String uri;
-        if(url.startsWith("<") && url.endsWith("</a>")) {
+        if (url.startsWith("<") && url.endsWith("</a>")) {
             uri = url.split(">", 2)[1].split("<", 2)[0];
         } else {
             uri = url;
         }
-        FittingContainer.get().getElementContainerProvider().navigateElementContainerTo(uri);
+        return uri;
     }
 }
